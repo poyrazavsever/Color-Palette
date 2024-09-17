23f40e87a7a2b221ffd3f8fe247bb58dcf9469f0
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-09-18 00:34:18 +0300

    feat: added layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<!-- src/routes/__layout.svelte -->
+<script>
+  import Footer from "../components/Footer.svelte";
+  import Navbar from "../components/Navbar.svelte";
+</script>
+
+<div class="relative">
+  <Navbar />
+
+  <main>
+    <slot></slot>
+  </main>
+
+  <Footer />
+</div>
