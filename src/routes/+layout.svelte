d6922bb4522fd2dd284f0d83c8558ca1cc74909a
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-09-18 00:35:56 +0300

    feat: added toast

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,13 @@
 <script>
   import Footer from "../components/Footer.svelte";
   import Navbar from "../components/Navbar.svelte";
+  import {Toaster} from "svelte-french-toast"
 </script>
 
 <div class="relative">
   <Navbar />
+  
+  <Toaster />
 
   <main>
     <slot></slot>
