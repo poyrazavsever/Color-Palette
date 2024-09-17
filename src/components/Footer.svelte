96deb4e41e978f3802690bf7897d5907db4f33c2
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-09-18 00:34:27 +0300

    feat: done footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css"
+</script>
+
+<footer class="absolute bottom-0 z-20 w-full h-8 bg-neutral-900">
+    <div class="flex w-full items-center justify-center">
+        <p class="text-lg font-semibold text-neutral-700">© 2024 Poyraz Avsever</p>
+    </div>
+
+</footer>
